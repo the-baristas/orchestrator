3f96a0e777c5494e18bf8c930665d7f1afc25774
Charles Kim <charles.kim@smoothstack.com>
2021-04-09 23:48:49 -0400

    Add create, update, and delete requests for airplane

--- src/main/java/com/utopia/orchestrator/OrchestratorController.java
+++ src/main/java/com/utopia/orchestrator/OrchestratorController.java
@@ -1,14 +1,20 @@
 package com.utopia.orchestrator;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class OrchestratorController {
+    private static final String FLIGHT_SERVICE_PATH = "http://flight-service";
+
     private final WebClient webClient;
 
     public OrchestratorController(WebClient webclient) {
@@ -17,14 +23,34 @@ public class OrchestratorController {
 
     @GetMapping("/airplanes")
     public ResponseEntity<String> findAllAirplanes() {
-        return webClient.get().uri("http://administration-service/airplanes")
+        return webClient.get().uri(FLIGHT_SERVICE_PATH + "/airplanes")
                 .retrieve().toEntity(String.class).block();
     }
 
     @GetMapping("/airplanes/{id}")
     public ResponseEntity<String> findAirplaneById(@PathVariable Long id) {
-        return webClient.get()
-                .uri("http://administration-service/airplanes/{id}", id)
+        return webClient.get().uri(FLIGHT_SERVICE_PATH + "airplanes/{id}", id)
                 .retrieve().toEntity(String.class).block();
+    }
+
+    @PostMapping("/airplanes")
+    public ResponseEntity<String> createAirplane(@RequestBody String json) {
+        return webClient.post().uri(FLIGHT_SERVICE_PATH + "/airplanes")
+                .contentType(MediaType.APPLICATION_JSON).bodyValue(json)
+                .retrieve().toEntity(String.class).block();
+    }
+
+    @PutMapping("/airplanes")
+    public ResponseEntity<String> updateAirplane(@RequestBody String json) {
+        return webClient.put().uri(FLIGHT_SERVICE_PATH + "/airplanes")
+                .contentType(MediaType.APPLICATION_JSON).bodyValue(json)
+                .retrieve().toEntity(String.class).block();
+    }
+
+    @DeleteMapping("/airplanes/{id}")
+    public ResponseEntity<String> deleteAirplane(@PathVariable Long id) {
+        return webClient.delete()
+                .uri(FLIGHT_SERVICE_PATH + "/airplanes/{id}", id).retrieve()
+                .toEntity(String.class).block();
     }
 }
