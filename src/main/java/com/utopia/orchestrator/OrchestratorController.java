9418e3b953717a23e4a6ea3c0541f0b5f2a4b21f
Charles Kim <charles.kim@smoothstack.com>
2021-04-10 22:05:20 -0400

    Add cross origin annotation to the controller

--- src/main/java/com/utopia/orchestrator/OrchestratorController.java
+++ src/main/java/com/utopia/orchestrator/OrchestratorController.java
@@ -2,6 +2,7 @@ package com.utopia.orchestrator;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@CrossOrigin
 @RestController
 public class OrchestratorController {
     private static final String FLIGHT_SERVICE_PATH = "http://flight-service";
