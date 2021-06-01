ac0b8e636da6f91b738d81ec50e6289f14cb4dcd
Charles Kim <charles.kim@smoothstack.com>
2021-06-01 01:20:23 -0400

    Customize EurekaIntanceConfigBean

--- src/main/java/com/utopia/orchestrator/OrchestratorApplication.java
+++ src/main/java/com/utopia/orchestrator/OrchestratorApplication.java
@@ -1,10 +1,15 @@
 package com.utopia.orchestrator;
 
+import com.netflix.appinfo.AmazonInfo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -34,5 +39,14 @@ public class OrchestratorApplication {
     @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
+    }
+
+    @Bean
+    @Profile("!default")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean bean = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        bean.setDataCenterInfo(info);
+        return bean;
     }
 }
